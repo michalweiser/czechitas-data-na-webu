bb865afef65a86a556c1044c02eee9156ad83ad4
Michal Weiser <michal.weiser@gooddata.com>
2016-10-19 14:28:24 +0200

    Working solution - unclean

--- README.md
+++ README.md
@@ -1,31 +1,20 @@
-# Node Todo App
+# Czechitas - Databáze, aneb ukládáme data na webu
 
-A Node app built with MongoDB and Angular. For demonstration purposes and a tutorial.
+Naucime se zaklady prace s daty v dokumentove NoSQL databazi MongoDB.
+Cilem je vytvoreni aplikace pro spravu ToDo listu.
 
-Node provides the RESTful API. Angular provides the frontend and accesses the API. MongoDB stores like a hoarder.
 
-## Requirements
+## Co budete potrebovat
 
-- [Node and npm](http://nodejs.org)
-- MongoDB: Make sure you have your own local or remote MongoDB database URI configured in `config/database.js`
+- [Git](https://git-scm.com/)
+- [Visual Studio Code](https://code.visualstudio.com)
+- [Node.js a npm](http://nodejs.org)
+- [MongoDB](https://www.mongodb.com/)
 
-## Installation
+## Instalace
 
 1. Clone the repository: `git clone git@github.com:scotch-io/node-todo`
 2. Install the application: `npm install`
 3. Place your own MongoDB URI in `config/database.js`
 3. Start the server: `node server.js`
 4. View in browser at `http://localhost:8080`
-
-## Tutorial Series
-
-This repo corresponds to the Node Todo Tutorial Series on [scotch.io](http://scotch.io)
-
-Each branch represents a certain tutorial.
-- tut1-starter: [Creating a Single Page Todo App with Node and Angular](http://scotch.io/tutorials/javascript/creating-a-single-page-todo-app-with-node-and-angular)
-- tut2-organization: [Application Organization and Structure](https://scotch.io/tutorials/node-and-angular-to-do-app-application-organization-and-structure)
-- tut3-services: [Controllers and Services](https://scotch.io/tutorials/node-and-angular-to-do-app-controllers-and-services)
-
-Happy Todo-ing!
-
-![Todo-aholic](http://i.imgur.com/ikyqgrn.png)
--- public/js/core.js
+++ public/js/core.js
@@ -1,1 +1,194 @@
-angular.module('scotchTodo', ['todoController', 'todoService']);
+var serialize = function (data) {
+  var keys = [];
+
+  Object.keys(data).forEach(function(key) {
+    if (data.hasOwnProperty(key)) {
+      keys.push(key + '=' + data[key]);
+    }
+  })
+
+  return keys.join('&');
+};
+
+var request = function request(type, url, data) {
+  var data = data || null;
+
+  return new Promise(function (resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(type, url, true);
+    xhr.onreadystatechange = function () {
+      var DONE = 4;
+      var OK = 200;
+      if (xhr.readyState === DONE) {
+        if (xhr.status === OK) {
+          resolve(JSON.parse(xhr.responseText));
+        } else {
+          reject('Error: ' + xhr.status);
+        }
+      }
+    };
+    if (data) {
+      xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+      xhr.send(serialize(data));
+    } else {
+      xhr.send();
+    }
+  });
+}
+
+// http service
+var http = {
+  get: function (url) {
+    return request('GET', url);
+  },
+  post: function (url, data) {
+    return request('POST', url, data);
+  },
+  delete: function (url) {
+    return request('DELETE', url);
+  }
+};
+
+// Todo Service
+var Service = function Service(http) {
+  this.get = function() {
+    return http.get('/api/todos');
+  };
+
+  this.create = function(todoData) {
+    return http.post('/api/todos', todoData);
+  };
+
+  this.delete = function(id) {
+    return http.delete('/api/todos/' + id);
+  };
+};
+
+var View = function View(scope) {
+  this.scope = scope;
+  this.spinner = scope.querySelector('.spinner');
+  this.input = scope.querySelector('.input');
+  this.submit = scope.querySelector('.submit');
+  this.todos = scope.querySelector('.todos');
+  this.length = scope.querySelector('.length');
+  this.form = scope.querySelector('form');
+
+  this.form.onsubmit = function (event) {
+    event.preventDefault();
+    this.onSubmit && this.onSubmit();
+    return false;
+  }.bind(this);
+
+  this.inputValue = function inputValue() {
+    return this.input.value;
+  }.bind(this);
+
+  this.cleanInput = function cleanInput() {
+    this.input.value = '';
+  };
+
+  this.isLoading = function isLoading(state) {
+    if (state) {
+      this.spinner.classList.remove('hidden');
+    } else {
+      this.spinner.classList.add('hidden');
+    }
+  };
+
+  this.createTodo = function (todo) {
+    var checkbox = document.createElement('div');
+
+    var label = document.createElement('label');
+    var input = document.createElement('input');
+    var text = document.createTextNode(todo.text);
+
+    checkbox.classList.add('checkbox');
+    input.type = 'checkbox';
+    input.onclick = function () {
+      this.deleteTodo(todo._id);
+    }.bind(this);
+
+    label.appendChild(input);
+    label.appendChild(text);
+    checkbox.appendChild(label);
+
+    return checkbox;
+  };
+
+  this.updateTodos = function (data) {
+    this.todos.querySelectorAll('.checkbox').forEach(function (todo) {
+      todo.parentElement.removeChild(todo);
+    });
+
+    data.forEach(function (todo) {
+      this.todos.appendChild(this.createTodo(todo));
+    }.bind(this));
+
+    this.length.textContent = data.length;
+  }
+
+  this.deleteTodo = function (id) {
+    this.onDeleteTodo && this.onDeleteTodo(id);
+  }
+};
+
+// Todo Controller
+var Control = function Control(view, service) {
+  this.loading = true;
+  this.todos = {};
+
+  view.onDeleteTodo = function(id) {
+    this.deleteTodo(id);
+  }.bind(this);
+
+  view.onSubmit = function() {
+    this.createTodo();
+  }.bind(this);
+
+  service.get().then(function (data) {
+    this.updateTodos(data);
+    this.isLoading(false);
+  }.bind(this));
+
+  this.createTodo = function createTodo() {
+    if (view.inputValue() !== '') {
+      this.isLoading(true);
+      service.create(this.createData()).then(function (data) {
+        this.isLoading(false);
+        view.cleanInput();
+        this.updateTodos(data);
+      }.bind(this));
+    }
+  };
+
+  this.createData = function createData() {
+    return {
+      text: view.input.value
+    };
+  };
+
+  this.deleteTodo = function deleteTodo(id) {
+    this.isLoading(true);
+    service.delete(id).then(function (data) {
+      this.isLoading(false);
+      this.updateTodos(data);
+    }.bind(this));
+  };
+
+  this.isLoading = function isLoading(state) {
+    this.loading = state;
+    view.isLoading(state);
+  }
+
+  this.updateTodos = function (data) {
+    this.todos = data;
+    view.updateTodos(data);
+  }
+};
+
+document.addEventListener('DOMContentLoaded', function init() {
+  window.todos = new Control(
+    new View(document.querySelector('body')),
+    new Service(http)
+  );
+}, false);
