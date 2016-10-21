e3217baa92c4cd44e426d21859d7650a63e41ace
Michal Weiser <michal.weiser@gooddata.com>
2016-10-21 03:59:06 +0200

    Update

--- server.js
+++ server.js
@@ -1,27 +1,40 @@
-// set up ======================================================================
 var express = require('express');
-var app = express(); 						// create our app w/ express
-var mongoose = require('mongoose'); 				// mongoose for mongodb
-var port = process.env.PORT || 8080; 				// set the port
-var database = require('./config/database'); 			// load the database config
+var app = express();
+var mongoose = require('mongoose');
+var port = process.env.PORT || 8080;
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
-// configuration ===============================================================
-mongoose.connect(database.localUrl); 	// Connect to local MongoDB instance. A remoteUrl is also available (modulus.io)
+var url = 'mongodb://localhost/czechitas';
 
-app.use(express.static('./public')); 		// set the static files location /public/img will be /img for users
-app.use(morgan('dev')); // log every request to the console
-app.use(bodyParser.urlencoded({'extended': 'true'})); // parse application/x-www-form-urlencoded
-app.use(bodyParser.json()); // parse application/json
-app.use(bodyParser.json({type: 'application/vnd.api+json'})); // parse application/vnd.api+json as json
-app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
+// var mongo = require('mongodb').MongoClient;
+// var assert = require('assert');
+// var ObjectId = require('mongodb').ObjectID;
 
+// mongo.connect(url, function(err, db) {
+//   assert.equal(null, err);
+//
+//   process.on('exit', function () {
+//     db.close();
+//   });
+//
+//   var cursor = db.collection('todos').find();
+//   cursor.toArray(function(err, docs) {
+//     console.log(docs);
+//   });
+// });
 
-// routes ======================================================================
+mongoose.connect(url);
+
+app.use(express.static('./public'));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extended': 'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
+app.use(methodOverride('X-HTTP-Method-Override'));
+
 require('./app/routes.js')(app);
 
-// listen (start app with node server.js) ======================================
 app.listen(port);
 console.log("App listening on port " + port);
