d519bacf7cc2c7f6612f32fe57561f5d7dcf672a
czechitas <czechitas>
2016-10-21 16:28:27 +0200

    workshop

--- app/routes.js
+++ app/routes.js
@@ -1,10 +1,47 @@
 var Todo = require('./models/todo');
 
 function getTodos(res) {
+    Todo.find(function (err, todos) {
+        if (err) {
+            res.send(err);
+        }
 
+        res.json(todos);
+    });
 };
 
 module.exports = function (app, db) {
+
+    app.get('/api/todos', function (req, res) {
+        getTodos(res);
+    });
+
+    app.post('/api/todos', function (req, res) {
+        Todo.create({
+            text: req.body.text
+        }, function (err, todo) {
+
+            if (err) {
+                res.send(err);
+            }
+
+            getTodos(res);
+        });
+
+    });
+
+    app.delete('/api/todos/:todo_id', function (req, res) {
+        Todo.remove({
+            _id: req.params.todo_id
+        }, function (err, todo) {
+
+            if (err) {
+                res.send(err);
+            }
+
+            getTodos(res);
+        });
+    });
 
     app.get('*', function (req, res) {
         res.sendFile(__dirname + '/public/index.html');
