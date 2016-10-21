66b3b66d747e0c8ddf033bed04e68306c35843fe
czechitas <czechitas>
2016-10-21 16:29:20 +0200

    workshop

--- app/models/todo.js
+++ app/models/todo.js
@@ -1,1 +1,9 @@
 var mongoose = require('mongoose');
+
+
+module.exports = mongoose.model('Todo', {
+    text: {
+        type: String,
+        default: ''
+    }
+});
--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.json({type: 'application/vnd.api+json'}));
 app.use(methodOverride('X-HTTP-Method-Override'));
 
-require('./app/routes.js')(app, db);
+require('./app/routes.js')(app);
 
 app.listen(port);
 console.log("App listening on port " + port);
